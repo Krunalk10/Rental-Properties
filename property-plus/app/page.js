c68ca2270f568c957527db28c794fba93a085d22
Krunal <krunalkamble4@gmail.com>
2024-07-06 09:46:34 +0530

    api created

--- property-plus/app/page.js
+++ property-plus/app/page.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import Hero from '@/components/Hero'
 import InfoBoxes from '@/components/InfoBoxes'
+import HomePageProperties from '@/components/HomePageProperties'
 
 const HomePage = () => {
   return (
       <>
         <Hero />
         <InfoBoxes />
+        <HomePageProperties />
       </>
   )
 }
 
-export default HomePage
+export default HomePage;
--- property-plus/app/properties/page.js
+++ property-plus/app/properties/page.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import properties from '@/properties.json';
-import PropertiyCard from '@/components/PropertiyCard';
-
+import PropertyCard from '@/components/PropertyCard';
 
 const PropertiesPage = () => {
   return (
-    <section class="px-4 py-6">
-      <div class="container-xl lg:container m-auto px-4 py-6">
-        { properties.length === 0 ? (
-          <p>No Properties Found</p>
-        ) : <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {properties.map((property) => (
-          <PropertiyCard key={property._id} property={property} />
-        ))}
-     </div>
-     }
-        
+    <section className="px-4 py-6">
+ <div className='container-xl lg:container m-auto px-4 py-6'>
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+
       </div>
     </section>
 
