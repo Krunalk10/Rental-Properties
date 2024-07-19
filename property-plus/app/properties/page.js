e02b24a37d94fd5f6480f98599cb328f76593b65
Krunal <krunalkamble4@gmail.com>
2024-07-19 16:16:35 +0530

    Search results completed

--- property-plus/app/properties/page.js
+++ property-plus/app/properties/page.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropertyCard from '@/components/PropertyCard';
 import { fetchProperties } from '@/utils/request';
+import PropertySearch from '@/components/PropertySearch';
 
 
 const PropertiesPage = async () => {
@@ -11,20 +12,28 @@ const PropertiesPage = async () => {
   properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
   
   return (
-    <section className="px-4 py-6">
-      <div className='container-xl lg:container m-auto px-4 py-6'>
-              {properties.length === 0 ? (
-                <p>No properties found</p>
-              ) : (
-                <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-                  {properties.map((property) => (
-                    <PropertyCard key={property._id} property={property} />
-                  ))}
-                </div>
-              )}
-      </div>
+    <>
+    <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+            <PropertySearch />
+        </div>
     </section>
-
+    
+   <section className="px-4 py-6">
+   <div className='container-xl lg:container m-auto px-4 py-6'>
+           {properties.length === 0 ? (
+             <p>No properties found</p>
+           ) : (
+             <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+               {properties.map((property) => (
+                 <PropertyCard key={property._id} property={property} />
+               ))}
+             </div>
+           )}
+   </div>
+ </section>
+  
+    </>
   )
 }
 
