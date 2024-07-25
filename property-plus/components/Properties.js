27f35955df9af72b62f69190ed911462bc3b244d
Krunal <krunalkamble4@gmail.com>
2024-07-25 13:24:46 +0530

    added pagination components

--- property-plus/components/Properties.js
+++ property-plus/components/Properties.js
@@ -1,49 +1,66 @@
-'use client'
-import React, { useEffect, useState } from 'react'
-import PropertyCard from './PropertyCard'
-import Spinner from './Spinner';
-import { fetchProperties } from '@/utils/request';
-
+'use client';
+import { useState, useEffect } from 'react';
+import PropertyCard from '@/components/PropertyCard';
+import Spinner from '@/components/Spinner';
+import Pagination from '@/components/Pagination';
 
 const Properties = () => {
-    const [properties, setProperties] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [properties, setProperties] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(6);
+  const [totalItems, setTotalItems] = useState(0);
 
-    useEffect(() => async () => {
-        try {
-            const res = await fetch('/api/properties');
+  useEffect(() => {
+    const fetchProperties = async () => {
+      try {
+        const res = await fetch(
+          `/api/properties?page=${page}&pageSize=${pageSize}`
+        );
 
-            if(!res.ok) {
-                throw new Error('Failed to fetch data');
-            }
-
-            const data = await res.json();
-            setProperties(data);
-
-        } catch (error) {
-            console.log(error);
-        } finally {
-           setLoading(false);
+        if (!res.ok) {
+          throw new Error('Failed to fetch data');
         }
 
-        fetchProperties();
-    }, []);
+        const data = await res.json();
+        setProperties(data.properties);
+        setTotalItems(data.total);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  return  loading ? (<Spinner /> ) : ( 
-    <section className="px-4 py-6">
-    <div className='container-xl lg:container m-auto px-4 py-6'>
-            {properties.length === 0 ? (
-              <p>No properties found</p>
-            ) : (
-              <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-                {properties.map((property) => (
-                  <PropertyCard key={property._id} property={property} />
-                ))}
-              </div>
-            )}
-    </div>
-  </section>
-  )
-}
+    fetchProperties();
+  }, [page, pageSize]);
 
-export default Properties
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+  };
+
+  return loading ? (
+    <Spinner />
+  ) : (
+    <section className='px-4 py-6'>
+      <div className='container-xl lg:container m-auto px-4 py-6'>
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+        <Pagination
+          page={page}
+          pageSize={pageSize}
+          totalItems={totalItems}
+          onPageChange={handlePageChange}
+        />
+      </div>
+    </section>
+  );
+};
+export default Properties;
