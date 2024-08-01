6058a729c8fcfa157ba0ec9c6e850bdbf21239c9
Krunal <krunalkamble4@gmail.com>
2024-08-01 11:05:02 +0530

    search

--- property-plus/app/properties/[id]/page.js
+++ property-plus/app/properties/[id]/page.js
@@ -1,5 +1,4 @@
-'use client' 
-
+'use client'
 import React from 'react'
 import { useEffect, useState } from 'react'
 import { useParams } from 'next/navigation'
@@ -15,43 +14,45 @@ import ShareButtons from '@/components/ShareButtons'
 import PropertyContactForm from '@/components/PropertyContactForm'
 
 const PropertyPage = () => {
-  
-  const {id} = useParams();
+
+  const { id } = useParams();
   const [property, setProperty] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  useEffect( () => {
+  useEffect(() => {
     const fetchPropertyData = async () => {
-      if (!id) return 
+      if (!id) return;
+
       try {
         const property = await fetchProperty(id);
         setProperty(property);
 
       } catch (error) {
         console.error('Error fetching property : ', error)
-      } 
-        finally {
-          setLoading(false);
-        }
-    }
-    if(property === null) {
-       fetchPropertyData();
-    }
-  },[id,property]);
+      }
+      finally {
+        setLoading(false);
+      }
+    };
 
-    if(!property && !loading) {
-      return (
-        <h1 className='text-center text-2xl font-bold mt-10'>
-          Property Not Found
-        </h1>
-      )
+    if (property === null) {
+      fetchPropertyData();
     }
+  }, [id, property]);
+
+  if (!property && !loading) {
+    return (
+      <h1 className='text-center text-2xl font-bold mt-10'>
+        Property Not Found
+      </h1>
+    )
+  }
   return (
     <>
-      {loading && <Spinner loading={loading}/>}
-      {!loading && property  && (
+      {loading && <Spinner loading={loading} />}
+      {!loading && property && (
         <>
-          <PropertyHeaderImage image = {property.images[0]} />  
+          <PropertyHeaderImage image={property.images[0]} />
           <section>
             <div className="container m-auto py-6 px-6">
               <Link
@@ -61,25 +62,25 @@ const PropertyPage = () => {
                 <FaArrowLeft className='mr-2' /> Back to Properties
               </Link>
             </div>
-         </section>
+          </section>
 
-         {/* <!-- Property Info --> */}
-    <section className="bg-blue-50">
-      <div className="container m-auto py-10 px-6">
-        <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-         <PropertyDetails property={property} />
+          {/* <!-- Property Info --> */}
+          <section className="bg-blue-50">
+            <div className="container m-auto py-10 px-6">
+              <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+                <PropertyDetails property={property} />
 
-          {/* <!-- Sidebar --> */}
-          <aside className="space-y-4">       
-              <BookmarkButton property={property} />
-              <ShareButtons property={property} />
-              <PropertyContactForm property={property} />            
-          </aside>
-        </div>
-      </div>
-    </section>
-    <PropertyImages images={property.images} />
-       </>
+                {/* <!-- Sidebar --> */}
+                <aside className="space-y-4">
+                  <BookmarkButton property={property} />
+                  <ShareButtons property={property} />
+                  <PropertyContactForm property={property} />
+                </aside>
+              </div>
+            </div>
+          </section>
+          <PropertyImages images={property.images} />
+        </>
       )}
     </>
   )
