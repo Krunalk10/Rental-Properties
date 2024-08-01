ef9758befbf75c039aac0c4969aebe05272dda55
Krunal <krunalkamble4@gmail.com>
2024-08-01 20:32:28 +0530

    layout

--- property-plus/app/api/properties/search/route.js
+++ property-plus/app/api/properties/search/route.js
@@ -1,45 +1,3 @@
-// import connectDB from '@/config/database';
-// import Property from '@/Models/Property';
-
-// // GET /api/properties/search
-// export const GET = async (request) => {
-//   try {
-//     await connectDB();
-
-//     const { searchParams } = new URL(request.url);
-//     const location = searchParams.get('location');
-//     const propertyType = searchParams.get('propertyType');
-
-//     const locationPattern = new RegExp(location, 'i');
-
-//     //Match location pattern against database fields
-//     let query = {
-//         $or : [
-//             { name : locationPattern},
-//             { description : locationPattern},
-//             { 'location.street' : locationPattern},
-//             { 'location.city' : locationPattern},
-//             { 'location.state' : locationPattern},
-//             { 'location.zipcode' : locationPattern},
-//            ] 
-//         }
-
-//     //Only check for properties if its not "All"
-//     if( propertyType && propertyType !== 'All' ) {
-//         const typePattern = new RegExp(propertyType, 'i');
-//         query.type = typePattern;
-//     }
-
-//     const properties = await Property.find(query);
-
-//     return new Response(JSON.stringify(properties), {status: 200});
-//   } catch (error) {
-//     console.log(error);
-//     return new Response('Something went wrong', { status: 500 });
-//   }
-// };
-
-
 import connectDB from '@/config/database';
 import Property from '@/Models/Property';
 
@@ -54,7 +12,7 @@ export const GET = async (request) => {
 
     const locationPattern = new RegExp(location, 'i');
 
-    // Match location pattern against database fields
+    //Match location pattern against database fields
     let query = {
       $or: [
         { name: locationPattern },
@@ -63,10 +21,10 @@ export const GET = async (request) => {
         { 'location.city': locationPattern },
         { 'location.state': locationPattern },
         { 'location.zipcode': locationPattern },
-      ],
-    };
+      ]
+    }
 
-    // Only check for property if its not 'All'
+    //Only check for properties if its not "All"
     if (propertyType && propertyType !== 'All') {
       const typePattern = new RegExp(propertyType, 'i');
       query.type = typePattern;
@@ -74,9 +32,7 @@ export const GET = async (request) => {
 
     const properties = await Property.find(query);
 
-    return new Response(JSON.stringify(properties), {
-      status: 200,
-    });
+    return new Response(JSON.stringify(properties), { status: 200 });
   } catch (error) {
     console.log(error);
     return new Response('Something went wrong', { status: 500 });
